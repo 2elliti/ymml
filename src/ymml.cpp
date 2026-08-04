a09926701eaf92759e13e82efc733debbf0f7d19
2elliti <forstoic724321@gmail.com>
2026-08-05 00:15:13 +0530

    fixes and chores

--- src/ymml.cpp
+++ src/ymml.cpp
@@ -4,11 +4,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
-#include <stdexcept>
 
-#define YMML_MEM_ALIGN 16
-#define YMML_MAX_GRAPH_TENSOR 1024
-#define YMML_PLACEHOLDER_SIZE 16
 #define YMML_PAD(x, n) (((x) + (n) - 1) & ~((n) - 1))
 
 struct ymml_type_info_t {
@@ -16,297 +12,302 @@ struct ymml_type_info_t {
   size_t type_size;
 };
 
-/*
-enum class ymml_type {
-  YMML_NONE = 0,
-  YMML_F16 = 1,
-  YMML_F32 = 2,
-  YMML_INT64 = 3,
-  YMML_END = 4
+static const ymml_type_info_t type_info[static_cast<int>(ymml_type::YMML_END)] =
+    {
+        {ymml_type::YMML_NONE, 0},
+        {ymml_type::YMML_F16, sizeof(uint16_t)},
+        {ymml_type::YMML_F32, sizeof(float)},
+        {ymml_type::YMML_INT64, sizeof(uint64_t)},
 };
-*/
-
-static struct ymml_type_info_t type_info[static_cast<int>(
-    ymml_type::YMML_END)] = {{ymml_type::YMML_NONE, 0},
-                             {ymml_type::YMML_F16, sizeof(uint16_t)},
-                             {ymml_type::YMML_F32, sizeof(float)},
-                             {ymml_type::YMML_INT64, sizeof(uint64_t)}};
 
 static void *aligned_malloc(size_t mem_size, size_t alignment) {
-  // lets make an extra space that we have to allocate.
-  size_t total_allocate_space = mem_size + alignment + sizeof(void *);
-  void *orignal_allocated_mem = malloc(total_allocate_space);
-
-  // Now we have to find next alligned address.
-  // for that we will use this formula: aligned = (raw + alignment - 1) &
-  // ~(alignment - 1)
-
-  // Keeping extra space for storing the original ptr.
-  uintptr_t orignal_allocated_mem_addr =
-      (uintptr_t)orignal_allocated_mem + sizeof(void *);
-
-  uintptr_t aligned_mem =
-      (orignal_allocated_mem_addr + alignment - 1) & ~(alignment - 1);
-  ((void **)aligned_mem)[-1] = orignal_allocated_mem;
-  return (void *)aligned_mem;
+  size_t total = mem_size + alignment + sizeof(void *);
+  void *raw = std::malloc(total);
+  if (!raw)
+    return nullptr;
+  uintptr_t after_slot = (uintptr_t)raw + sizeof(void *);
+  uintptr_t aligned =
+      (after_slot + alignment - 1) & ~(uintptr_t)(alignment - 1);
+  ((void **)aligned)[-1] = raw;
+  return (void *)aligned;
 }
 
-static void aligned_free(void *p) { free(((void **)p)[-1]); }
+static void aligned_free(void *p) {
+  if (!p)
+    return;
+  std::free(((void **)p)[-1]);
+}
 
-/*
- Aim: Has to return allocated arena for metadata.
-*/
-ymml_meta_data_arena *ymml_init_meta_arena(ymml_meta_data_param &param) {
-  // Returns meta_data_arena,But  its possible that user might have already
-  // allocated the  buffer. Why ? allocated arena might be on heterogeneous
-  // hardware therefore  keep it  in checkkkkkkkkkkkk. Therefore  check if param
-  // already  has allocated buffer or not.
-
-  // First check if memsize is 0?
-  if (param.mem_size == 0) {
+static ymml_arena *arena_init_common(ymml_arena_param &param) {
+  if (param.mem_size == 0)
     param.mem_size = YMML_MEM_ALIGN;
-  }
 
   const size_t mem_size =
       param.buffer ? param.mem_size : YMML_PAD(param.mem_size, YMML_MEM_ALIGN);
 
-  ymml_meta_data_arena *ctx =
-      (ymml_meta_data_arena *)malloc(sizeof(ymml_meta_data_arena));
+  auto *ctx = (ymml_arena *)std::malloc(sizeof(ymml_arena));
+  YMML_ASSERT(ctx != nullptr);
+
   ctx->mem_size = mem_size;
   ctx->mem_buffer =
       param.buffer ? param.buffer : aligned_malloc(mem_size, YMML_MEM_ALIGN);
+  ctx->owns_buffer = (param.buffer == nullptr);
   ctx->begin = nullptr;
   ctx->end = nullptr;
   ctx->total_objects = 0;
-  assert(ctx->mem_buffer != nullptr);
+
+  YMML_ASSERT(ctx->mem_buffer != nullptr);
   return ctx;
 }
 
-/*
-  Aim: Returns allocated arena for payload.
-*/
-
-ymml_data_arena *ymml_init_data_arena(ymml_data_param &param) {
-  // Returns the arena. Check if its already allocated or not.
-
-  if (param.mem_size == 0) {
-    param.mem_size = YMML_MEM_ALIGN;
-  }
-  const size_t mem_size =
-      param.buffer ? param.mem_size : YMML_PAD(param.mem_size, YMML_MEM_ALIGN);
-
-  ymml_data_arena *ctx = (ymml_data_arena *)malloc(sizeof(ymml_data_arena));
-  ctx->mem_size = mem_size;
-  ctx->mem_buffer =
-      param.buffer ? param.buffer : aligned_malloc(mem_size, YMML_MEM_ALIGN);
-  ctx->begin = nullptr;
-  ctx->end = nullptr;
-  ctx->total_objects = 0;
-  return ctx;
+ymml_arena *ymml_init_meta_arena(ymml_arena_param &param) {
+  return arena_init_common(param);
 }
 
-static struct ymml_object *
-ymml_new_meta_object(struct ymml_meta_data_arena *marena,
-                     enum ymml_object_type type, size_t size) {
-  struct ymml_object *object = marena->end;
-  size_t last_obj_offset = object ? object->offset : 0;
-  size_t last_obj_size = object ? object->size : 0;
-  size_t end = last_obj_size + last_obj_offset;
-  ymml_object *n_obj =
-      (struct ymml_object *)((uintptr_t)marena->mem_buffer + end);
-  marena->total_objects++;
-  n_obj->offset = end + YMML_OBJECT_SIZE;
-  n_obj->size = size;
-  n_obj->unified_type.meta_type = type;
-  n_obj->next = NULL;
-  if (marena->end) {
-    marena->end->next = n_obj;
-  } else {
-    marena->begin = n_obj;
-  }
-  marena->end = n_obj;
-  return n_obj;
+ymml_arena *ymml_init_data_arena(ymml_arena_param &param) {
+  return arena_init_common(param);
 }
 
-static struct ymml_tensor *
-ymml_new_tensor_impl(ymml_meta_data_arena *meta_arena, enum ymml_type type,
-                     int dims, uint64_t *ne) {
-  // For this tensor allocate struct on meta data arena and buffer in data
-  // arena.
-  assert(dims >= 1 && dims <= YMML_MAX_DIMENSIONS);
-
-  // start calculating total bytes needed to store in buffer.
-  uint64_t total_elements = ne[0];
-  for (int i = 1; i < dims; i++) {
-    total_elements *= ne[i];
-  }
-
-  // Calculate number of bytes required for storing buffer data.
-  uint64_t total_bytes =
-      type_info[static_cast<uint64_t>(type)].type_size * total_elements;
-
-  // We need a new way of thinking. WHat if i make or carve space out of the
-  // buffer?
-  struct ymml_object *tensor_object = ymml_new_meta_object(
-      meta_arena, ymml_object_type::YMML_OBJ_TYP_TENSOR, YMML_TENSOR_SIZE);
-
-  // Try getting tensor pointer.
-
-  struct ymml_tensor *n_tensor =
-      (struct ymml_tensor *)((uintptr_t)meta_arena->mem_buffer +
-                             tensor_object->offset);
-  for (uint32_t i = 0; i < dims; i++) {
-    n_tensor->ne[i] = ne[i];
-  }
-
-  // Initialize unused dimensions as 1.
-  for (uint32_t i = dims; i < YMML_MAX_DIMENSIONS; i++)
-    n_tensor->ne[i] = 1;
-
-  // Initialize all strides as 0.
-  for (uint32_t i = 0; i < YMML_MAX_DIMENSIONS; i++)
-    n_tensor->nb[i] = 0;
-
-  // Initialize all src ptx as null.
-  for (uint32_t i = 0; i < YMML_MAX_SRC; i++)
-    n_tensor->src[i] = nullptr;
-  n_tensor->dims = dims;
-  n_tensor->type = type;
-  n_tensor->visited = false;
-
-  // Keep operations null here.
-  n_tensor->op = ymml_op::YMML_OP_NONE;
-  return n_tensor;
+void ymml_free_arena(ymml_arena *arena) {
+  if (!arena)
+    return;
+  if (arena->owns_buffer)
+    aligned_free(arena->mem_buffer);
+  std::free(arena);
 }
 
-struct ymml_tensor *ymml_new_tensor(ymml_meta_data_arena *meta_arena,
-                                    enum ymml_type type, int dims,
-                                    uint64_t *ne) {
-
-  return ymml_new_tensor_impl(meta_arena, type, dims, ne);
+void ymml_arena_reset(ymml_arena *arena) {
+  arena->begin = nullptr;
+  arena->end = nullptr;
+  arena->total_objects = 0;
 }
 
-static struct ymml_object *
-ymml_new_data_object(struct ymml_data_arena *data_arena, enum ymml_type type,
-                     void *data, size_t size) {
-  struct ymml_object *object = data_arena->end;
-  size_t last_obj_size = object ? object->size : 0;
-  size_t last_obj_offset = object ? object->offset : 0;
-  size_t end = last_obj_offset + last_obj_size;
+static ymml_object *arena_bump(ymml_arena *arena, size_t payload_size) {
+  const size_t last_end =
+      arena->end ? (arena->end->offset + arena->end->size) : 0;
 
-  struct ymml_object *n_obj =
-      (struct ymml_object *)((uintptr_t)data_arena->mem_buffer + end);
-  data_arena->total_objects++;
-  n_obj->offset = end + YMML_OBJECT_SIZE;
-  n_obj->size = size;
-  n_obj->unified_type.data_type = type;
-  char *base_ptr = (char *)data_arena->mem_buffer + n_obj->offset;
+  const size_t header_off = YMML_PAD(last_end, YMML_MEM_ALIGN);
+  const size_t payload_off =
+      YMML_PAD(header_off + YMML_OBJECT_SIZE, YMML_MEM_ALIGN);
+  const size_t padded_size = YMML_PAD(payload_size, YMML_MEM_ALIGN);
+  const size_t new_end = payload_off + padded_size;
 
-  // This here is worst thing, we ever did. Get this shit done.
-  if (data != nullptr)
-    memcpy(base_ptr, data, size);
-
-  if (data_arena->end) {
-    data_arena->end->next = n_obj;
-  } else {
-    data_arena->begin = n_obj;
+  if (new_end > arena->mem_size) {
+    std::fprintf(
+        stderr, "ymml: arena OOM (need %zu, have %zu, requested payload=%zu)\n",
+        new_end, arena->mem_size, payload_size);
+    return nullptr;
   }
 
-  data_arena->end = n_obj;
-  return n_obj;
+  auto *obj = (ymml_object *)((uintptr_t)arena->mem_buffer + header_off);
+  obj->offset = payload_off;
+  obj->size = padded_size;
+  obj->next = nullptr;
+
+  if (arena->end)
+    arena->end->next = obj;
+  else
+    arena->begin = obj;
+  arena->end = obj;
+  arena->total_objects++;
+
+  return obj;
 }
 
-void ymml_fill_tensor_data(struct ymml_data_arena *data_arena,
-                           struct ymml_tensor *tensor, void *data,
-                           size_t size) {
-  /*
-    What is the aim for this function?
-    What exactly do i have to do now?
-    You have a data pointer.
-    You have a total size.
-  */
+static ymml_object *ymml_new_meta_object(ymml_arena *marena,
+                                         ymml_object_type type, size_t size) {
+  ymml_object *obj = arena_bump(marena, size);
+  if (!obj)
+    YMML_ABORT("meta arena OOM");
+  obj->unified_type.meta_type = type;
+  return obj;
+}
 
-  // calculate the size of this shit.
-  size_t total_bytes_req =
-      type_info[static_cast<int>(tensor->type)].type_size * size;
-  struct ymml_object *obj =
-      ymml_new_data_object(data_arena, tensor->type, data, total_bytes_req);
+static ymml_object *ymml_new_data_object(ymml_arena *darena, ymml_type type,
+                                         const void *src, size_t size) {
+  ymml_object *obj = arena_bump(darena, size);
+  if (!obj)
+    YMML_ABORT("data arena OOM");
+  obj->unified_type.data_type = type;
+
+  if (src != nullptr) {
+    void *dst = (char *)darena->mem_buffer + obj->offset;
+    std::memcpy(dst, src, size);
+  }
+  return obj;
+}
+
+size_t ymml_nelements(const ymml_tensor *t) {
+  uint64_t n = 1;
+  for (int i = 0; i < t->dims; i++)
+    n *= t->ne[i];
+  return (size_t)n;
+}
+
+size_t ymml_nbytes(const ymml_tensor *t) {
+  return ymml_nelements(t) * type_info[(int)t->type].type_size;
+}
+
+bool ymml_is_contiguous(const ymml_tensor *t) {
+  if (t->nb[0] != type_info[(int)t->type].type_size)
+    return false;
+  for (int i = 1; i < YMML_MAX_DIMENSIONS; i++) {
+    if (t->nb[i] != t->nb[i - 1] * t->ne[i - 1])
+      return false;
+  }
+  return true;
+}
+
+static ymml_tensor *ymml_new_tensor_impl(ymml_arena *marena, ymml_type type,
+                                         int dims, uint64_t *ne) {
+  YMML_ASSERT(dims >= 1 && dims <= YMML_MAX_DIMENSIONS);
+  YMML_ASSERT((int)type > 0 && (int)type < (int)ymml_type::YMML_END);
+
+  ymml_object *obj = ymml_new_meta_object(
+      marena, ymml_object_type::YMML_OBJ_TYP_TENSOR, YMML_TENSOR_SIZE);
+
+  auto *t = (ymml_tensor *)((uintptr_t)marena->mem_buffer + obj->offset);
+
+  t->type = type;
+  t->dims = (uint8_t)dims;
+  t->op = ymml_op::YMML_OP_NONE;
+
+  for (int i = 0; i < dims; i++)
+    t->ne[i] = ne[i];
+  for (int i = dims; i < YMML_MAX_DIMENSIONS; i++)
+    t->ne[i] = 1;
+
+  t->nb[0] = type_info[(int)type].type_size;
+  for (int i = 1; i < YMML_MAX_DIMENSIONS; i++)
+    t->nb[i] = t->nb[i - 1] * t->ne[i - 1];
+
+  for (int i = 0; i < YMML_MAX_SRC; i++)
+    t->src[i] = nullptr;
+
+  t->data = nullptr;
+  t->data_object = nullptr;
+  t->name[0] = '\0';
+
+  return t;
+}
+
+ymml_tensor *ymml_new_tensor(ymml_arena *marena, ymml_type type, int dims,
+                             uint64_t *ne) {
+  return ymml_new_tensor_impl(marena, type, dims, ne);
+}
+
+void ymml_fill_tensor_data(ymml_arena *darena, ymml_tensor *tensor, void *data,
+                           size_t n_elements) {
+  YMML_ASSERT(n_elements == ymml_nelements(tensor));
+  const size_t nbytes = type_info[(int)tensor->type].type_size * n_elements;
+
+  ymml_object *obj = ymml_new_data_object(darena, tensor->type, data, nbytes);
   tensor->data_object = obj;
-  tensor->data = (void *)(((char *)data_arena->mem_buffer + obj->offset));
+  tensor->data = (char *)darena->mem_buffer + obj->offset;
 }
 
-struct ymml_object *ymml_get_tensor_data(struct ymml_tensor *tensor) {
+ymml_object *ymml_get_tensor_data(ymml_tensor *tensor) {
   return tensor->data_object;
 }
 
-// returns a duplicate tensor.
-static struct ymml_tensor *
-ymml_tensor_duplicate(struct ymml_meta_data_arena *marena,
-                      struct ymml_tensor *tensor) {
-  return ymml_new_tensor(marena, tensor->type, tensor->dims, tensor->ne);
+static ymml_tensor *ymml_tensor_dup(ymml_arena *marena, ymml_tensor *t) {
+  return ymml_new_tensor(marena, t->type, t->dims, t->ne);
 }
 
-struct ymml_tensor *ymml_add(struct ymml_meta_data_arena *marena,
-                             struct ymml_data_arena *data,
-                             struct ymml_tensor *a, struct ymml_tensor *b,
-                             bool inplace) {
+ymml_tensor *ymml_add(ymml_arena *marena, ymml_arena * /*darena*/,
+                      ymml_tensor *a, ymml_tensor *b, bool inplace) {
+  YMML_ASSERT(a && b);
+  YMML_ASSERT(a->type == b->type);
+  for (int i = 0; i < YMML_MAX_DIMENSIONS; i++)
+    YMML_ASSERT(a->ne[i] == b->ne[i]);
 
-  // we need to do some testing
-  // need meta arena, whats the type? check the dims and ne.
-  // We will give 2 options here, whether a user wants to go for inplace or not?
-
-  struct ymml_tensor *ntensor = ymml_tensor_duplicate(marena, a);
-  ntensor->op = ymml_op::YMML_OP_ADD;
-  ntensor->src[0] = a;
-  ntensor->src[1] = b;
-  return ntensor;
-}
-
-static ymml_graph *ymml_new_graph_impl(struct ymml_meta_data_arena *marena) {
-  // Get the object (This keeps record of meta data).
-  struct ymml_object *obj = ymml_new_meta_object(
-      marena, ymml_object_type::YMML_OBJ_TYP_GRAPH, YMML_GRAPH_SIZE);
-  // Build struct for graph and return.
-  return (struct ymml_graph *)((uintptr_t)marena->mem_buffer + obj->offset);
-}
-
-struct ymml_graph *ymml_new_graph(struct ymml_meta_data_arena *marena,
-                                  struct ymml_data_arena *darena) {
-  // Get the bare structure of graph.
-  struct ymml_graph *ngraph = ymml_new_graph_impl(marena);
-
-  // allocates space for sorted nodes.
-  struct ymml_object *object = ymml_new_data_object(
-      darena, ymml_type::YMML_INT64, nullptr, YMML_MAX_GRAPH_TENSOR);
-
-  // Assign the allocated space to ngraph.
-  ngraph->sorted_nodes =
-      (struct ymml_tensor **)((uintptr_t)darena->mem_buffer + object->offset);
-
-  // Keep start-node as nullptr.
-  ngraph->start_node = nullptr;
-
-  // Keep total nodes as 0
-  ngraph->total_nodes = 0;
-
-  return ngraph;
-}
-
-static void ymml_visit_parent_nodes(struct ymml_graph *graph,
-                                    struct ymml_tensor *tensor) {
-  if (tensor == nullptr)
-    return;
-
-  for (uint8_t i = 0; i < YMML_MAX_SRC; i++) {
-    ymml_visit_parent_nodes(graph, tensor->src[i]);
+  ymml_tensor *out;
+  if (inplace) {
+    out = ymml_tensor_dup(marena, a);
+    out->data = a->data;
+    out->data_object = a->data_object;
+  } else {
+    out = ymml_tensor_dup(marena, a);
   }
-  graph->sorted_nodes[graph->total_nodes] = tensor;
-  graph->total_nodes++;
+  out->op = ymml_op::YMML_OP_ADD;
+  out->src[0] = a;
+  out->src[1] = b;
+  return out;
 }
 
-void ymml_build_forward_graph(struct ymml_graph *graph,
-                              struct ymml_tensor *tensor) {
-  if (tensor == nullptr)
+static inline size_t hash_ptr(const void *p) {
+  uintptr_t x = (uintptr_t)p;
+  x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+  x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+  x = x ^ (x >> 31);
+  return (size_t)x;
+}
+
+static bool visited_insert(ymml_graph *g, ymml_tensor *t) {
+  const size_t mask = g->visited_cap - 1;
+  size_t i = hash_ptr(t) & mask;
+  for (;;) {
+    if (g->visited[i] == nullptr) {
+      g->visited[i] = t;
+      g->visited_count++;
+      return true;
+    }
+    if (g->visited[i] == t)
+      return false;
+    i = (i + 1) & mask;
+  }
+}
+
+static void ymml_visit(ymml_graph *g, ymml_tensor *t) {
+  if (!t)
     return;
-  ymml_visit_parent_nodes(graph, tensor);
+  if (!visited_insert(g, t))
+    return;
+
+  for (int i = 0; i < YMML_MAX_SRC; i++)
+    ymml_visit(g, t->src[i]);
+
+  YMML_ASSERT(g->total_nodes < g->capacity);
+  g->sorted_nodes[g->total_nodes++] = t;
+}
+
+ymml_graph *ymml_new_graph(ymml_arena *marena) {
+  ymml_object *gobj = ymml_new_meta_object(
+      marena, ymml_object_type::YMML_OBJ_TYP_GRAPH, YMML_GRAPH_SIZE);
+  auto *g = (ymml_graph *)((uintptr_t)marena->mem_buffer + gobj->offset);
+
+  const size_t cap = YMML_MAX_GRAPH_NODE;
+  ymml_object *nodes_obj =
+      ymml_new_meta_object(marena, ymml_object_type::YMML_OBJ_TYP_OBJECT,
+                           cap * sizeof(ymml_tensor *));
+  g->sorted_nodes =
+      (ymml_tensor **)((uintptr_t)marena->mem_buffer + nodes_obj->offset);
+  g->capacity = cap;
+  g->total_nodes = 0;
+  g->start_node = nullptr;
+
+  size_t vcap = 1;
+  while (vcap < cap * 2)
+    vcap <<= 1;
+  ymml_object *vobj =
+      ymml_new_meta_object(marena, ymml_object_type::YMML_OBJ_TYP_OBJECT,
+                           vcap * sizeof(ymml_tensor *));
+  g->visited = (ymml_tensor **)((uintptr_t)marena->mem_buffer + vobj->offset);
+  g->visited_cap = vcap;
+  g->visited_count = 0;
+  std::memset(g->visited, 0, vcap * sizeof(ymml_tensor *));
+
+  return g;
+}
+
+void ymml_build_forward_graph(ymml_graph *graph, ymml_tensor *root) {
+  if (!root)
+    return;
+
+  graph->total_nodes = 0;
+  graph->visited_count = 0;
+  std::memset(graph->visited, 0, graph->visited_cap * sizeof(ymml_tensor *));
+
+  graph->start_node = root;
+  ymml_visit(graph, root);
 }
--- src/ymml.h
+++ src/ymml.h
@@ -2,6 +2,9 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+
 #ifdef YMML_SHARED
 #define YMML_API __attribute__((visibility("default"))) extern
 #else
@@ -9,9 +12,23 @@
 #endif
 
 #define YMML_MAX_DIMENSIONS 4
-#define YMML_MAX_SRC 10
+#define YMML_MAX_SRC 4
 #define YMML_MAX_NAME 32
-#define YMML_MAX_GRAPH_NODE 1024
+#define YMML_MAX_GRAPH_NODE 8192
+#define YMML_MEM_ALIGN 64
+
+#define YMML_ABORT(msg)                                                        \
+  do {                                                                         \
+    std::fprintf(stderr, "ymml abort: %s (%s:%d)\n", (msg), __FILE__,          \
+                 __LINE__);                                                    \
+    std::abort();                                                              \
+  } while (0)
+
+#define YMML_ASSERT(cond)                                                      \
+  do {                                                                         \
+    if (!(cond))                                                               \
+      YMML_ABORT("assertion failed: " #cond);                                  \
+  } while (0)
 
 typedef uint16_t ymml_fp16_t;
 
@@ -22,14 +39,18 @@ enum class ymml_type {
   YMML_INT64 = 3,
   YMML_END = 4
 };
-enum class ymml_op { YMML_OP_ADD, YMML_OP_NONE };
+
+enum class ymml_op {
+  YMML_OP_NONE = 0,
+  YMML_OP_ADD,
+};
+
 enum class ymml_object_type {
   YMML_OBJ_TYP_TENSOR,
   YMML_OBJ_TYP_GRAPH,
   YMML_OBJ_TYP_OBJECT
 };
 
-// Stores offset, size and type of object in arena buffer.
 struct ymml_object {
   size_t offset;
   size_t size;
@@ -40,110 +61,76 @@ struct ymml_object {
   } unified_type;
 };
 
-static const size_t YMML_OBJECT_SIZE = sizeof(struct ymml_object);
+constexpr size_t YMML_OBJECT_SIZE = sizeof(struct ymml_object);
 
-struct ymml_meta_data_param {
-  size_t mem_size;
-  void *buffer;
-};
-
-struct ymml_data_param {
-  size_t mem_size;
-  void *buffer;
-};
-
-struct ymml_meta_data_arena {
+struct ymml_arena {
   size_t mem_size;
   void *mem_buffer;
-  int total_objects = 0;
-
-  // Keeps track of start and end of linked list
+  bool owns_buffer;
+  int total_objects;
   struct ymml_object *begin;
   struct ymml_object *end;
 };
 
-struct ymml_data_arena {
+struct ymml_arena_param {
   size_t mem_size;
-  void *mem_buffer;
-  int total_objects = 0;
-
-  // Keeps track of start and end of linked list
-  struct ymml_object *begin;
-  struct ymml_object *end;
+  void *buffer;
 };
+
+using ymml_meta_data_arena = ymml_arena;
+using ymml_data_arena = ymml_arena;
+using ymml_meta_data_param = ymml_arena_param;
+using ymml_data_param = ymml_arena_param;
 
 struct ymml_tensor {
   enum ymml_type type;
-  char name[YMML_MAX_NAME];
-  uint64_t ne[YMML_MAX_DIMENSIONS];
-  uint32_t nb[YMML_MAX_DIMENSIONS];
-
-  // Operation for this tensor.
-  enum ymml_op op;
-
-  // Parent tensors.
-  struct ymml_tensor *src[YMML_MAX_SRC];
-
-  // Stores the tensor data.
-  void *data;
-
-  // Question do we really need tensor data?
-  // Or do we need a mechanism for storing info in other arena?
-  struct ymml_object *data_object;
-
   uint8_t dims;
-
-  bool visited;
+  enum ymml_op op;
+  uint64_t ne[YMML_MAX_DIMENSIONS];
+  size_t nb[YMML_MAX_DIMENSIONS];
+  void *data;
+  struct ymml_tensor *src[YMML_MAX_SRC];
+  struct ymml_object *data_object;
+  char name[YMML_MAX_NAME];
 };
 
-static const size_t YMML_TENSOR_SIZE = sizeof(struct ymml_tensor);
+constexpr size_t YMML_TENSOR_SIZE = sizeof(struct ymml_tensor);
 
-YMML_API ymml_meta_data_arena *
-ymml_init_meta_arena(ymml_meta_data_param &param);
+struct ymml_graph {
+  size_t total_nodes;
+  size_t capacity;
+  struct ymml_tensor *start_node;
+  struct ymml_tensor **sorted_nodes;
+  struct ymml_tensor **visited;
+  size_t visited_cap;
+  size_t visited_count;
+};
 
-YMML_API ymml_data_arena *ymml_init_data_arena(ymml_data_param &param);
+constexpr size_t YMML_GRAPH_SIZE = sizeof(struct ymml_graph);
 
-YMML_API struct ymml_tensor *ymml_new_tensor(ymml_meta_data_arena *meta_arena,
+YMML_API ymml_arena *ymml_init_meta_arena(ymml_arena_param &param);
+YMML_API ymml_arena *ymml_init_data_arena(ymml_arena_param &param);
+YMML_API void ymml_free_arena(ymml_arena *arena);
+YMML_API void ymml_arena_reset(ymml_arena *arena);
+
+YMML_API struct ymml_tensor *ymml_new_tensor(ymml_arena *marena,
                                              enum ymml_type type, int dims,
                                              uint64_t *ne);
 
-YMML_API void ymml_fill_tensor_data(struct ymml_data_arena *data_arena,
+YMML_API void ymml_fill_tensor_data(ymml_arena *darena,
                                     struct ymml_tensor *tensor, void *data,
-                                    size_t size);
+                                    size_t n_elements);
 
 YMML_API struct ymml_object *ymml_get_tensor_data(struct ymml_tensor *tensor);
 
-struct ymml_tensor *ymml_add(struct ymml_meta_data_arena *mdata,
-                             struct ymml_data_arena *data,
-                             struct ymml_tensor *a, struct ymml_tensor *b,
-                             bool inplace);
+YMML_API bool ymml_is_contiguous(const struct ymml_tensor *t);
+YMML_API size_t ymml_nbytes(const struct ymml_tensor *t);
+YMML_API size_t ymml_nelements(const struct ymml_tensor *t);
 
-/*
-  Why ymml-graph?
-  we  need  to convert  our tensor  and its resources into DAG (Directed acyclic
-  graph) to execute  it.
+YMML_API struct ymml_tensor *ymml_add(ymml_arena *marena, ymml_arena *darena,
+                                      struct ymml_tensor *a,
+                                      struct ymml_tensor *b, bool inplace);
 
-  How are we going to do ?
-
-  We  first decide what are the members that we need.
-
-  - number  of nodes.
-  - pointers to all the tensors.aa
-*/
-
-/*
-How we tackling this?
-
-*/
-struct ymml_graph {
-  // We need to  topo sort  all the tensors.
-  // need a hash table for storing info wether we visited a node or not?
-  size_t total_nodes;
-  struct ymml_tensor *start_node;
-  struct ymml_tensor **sorted_nodes;
-  // Do i add a hash table in here?
-};
-
-static const size_t YMML_GRAPH_SIZE = sizeof(struct ymml_graph);
-void ymml_build_forward_graph(struct ymml_graph *graph, struct ymml_tensor *tensor);
-struct ymml_graph * ymml_new_graph(struct ymml_meta_data_arena *marena, struct ymml_data_arena *darena);
+YMML_API struct ymml_graph *ymml_new_graph(ymml_arena *marena);
+YMML_API void ymml_build_forward_graph(struct ymml_graph *graph,
+                                       struct ymml_tensor *root);
